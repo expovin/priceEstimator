44dd7fb3f609ce9674138ae5de6a288f9dd7f69e
Vincenzo Esposito <expovin@gmail.com>
2019-06-12 17:30:05 +0200

    Use of serializeapp

--- Classes/QIX.js
+++ Classes/QIX.js
@@ -1,4 +1,5 @@
 const enigma = require('enigma.js');
+var serializeapp = require('serializeapp')
 const enigmaConfig = require('../conf/enigma');
 const Config = require('../conf/settings');
 const settings = new Config();
@@ -7,12 +8,15 @@ class QIX {
 
     constructor(MonApp=settings.QIX.OPMON){
         this.MonApp=MonApp;
-        this.appObj;
+        this.qdoc;
         this.fieldObj;
         this.objId;
-        this.DimensionId=[];
+        this.serializedApp;
+        this.qix;
         console.log("Instanziato Classe QIX "+this.MonApp);
     }
+
+
     init(){
         console.log("Called OpenEngine");
         return new Promise ( (fulfill, reject) => {
@@ -20,10 +24,50 @@ class QIX {
             .open()
             .then((qix) => {
                 console.log("QIX Engine correctly opened");
-                this.qix=qix;                
-                fulfill(qix);
-            }, error => reject(error) );
+                this.qix=qix;  
+                return this.qix.openDoc({qDocName:this.MonApp})
+            }, err => { reject("Error opening Document :"+err); })
+            .then(qdoc => {
+                this.qdoc=qdoc;
+                return this.serialize();
+            }, err => { reject("Error serializing Document :"+err); })
+            .then(data =>{
+                this.serializedApp=data;
+                fulfill("Initialization done");
+            })
+            .catch(error => reject(error))
         })
+    }
+
+    getDimensions() { return this.serializedApp.dimensions }
+    getMeasures() {return this.serializedApp.measures }
+    getSheets() { return this.serializedApp.sheets }
+    getStories() { return this.serializedApp.stories }
+    getMasterObjects() { return this.serializedApp.masterobjects }
+    getDataConnections() { return this.serializedApp.dataconnections }
+    getBookmarks() {return this.serializedApp.bookmarks }
+    getEmbeddedmedias() {return this.serializedApp.embeddedmedia }
+    getFields() {return this.serializedApp.embeddedmedia }
+    getSnapshoots() { return this.serializedApp.snapshots }
+    getProperties(){ return this.serializedApp.properties }
+    getLoadscript() { return this.serializedApp.loadscript }
+
+    getDimensionByName(name) { return this.serializedApp.dimensions.filter( element => { return element.qDim.title === name })}
+    getMeasureByName(name) { return this.serializedApp.measures.filter( element => { return element.qMeasure.qLabel === name })}
+    getSheetByName(name) { return this.serializedApp.sheets.filter( element => { return element.qProperty.qMetaDef.title === name })}
+    getMasterObjectByName(name) { return this.serializedApp.masterobjects.filter( element => { return element.qProperty.qMetaDef.title === name })}
+    getDataConnectionByName(name) { return this.serializedApp.dataconnections.filter( element => { return element.qName === name })}
+
+
+
+
+    serialize(){
+        return new Promise( (fulfill, reject) =>{
+            serializeapp(this.qdoc)
+            .then( info => fulfill(info))
+            .catch ( error => reject(error))
+        })
+        
     }
 
     closeEngine(){
@@ -56,21 +100,7 @@ class QIX {
         })
     }
 
-    openDoc(){
-        console.log("App da aprire "+this.MonApp);
-        return new Promise( (fulfill, reject) =>{
-            this.qix.openDoc({qDocName:this.MonApp})
-            .then(qdoc => {
-                this.appObj=qdoc;
-                fulfill({data:"Doc Opened",appObj:this.appObj});
-            }, err => {
-                reject(err);
-            })
-            .catch(error => {
-                reject(error);
-            })
-        })
-    }
+
 
     getAllInfos() {  
         return new Promise ( (fulfill, reject) =>{
--- test.js
+++ test.js
@@ -2,46 +2,9 @@ var QIX = require ('./Classes/QIX');
 
 qix = new QIX();
 
-console.log(qix);
-
 qix.init()
-.then ( result =>{
-    return (qix.getEngineVersion())
-})
-.then( version =>{
-    console.log("Connected to the instance ver." + version.qComponentVersion);
-    return qix.openDoc();
-})
-
-.then( doc =>{
-    return qix.getAllInfos();
-})
-
-.then(info =>{
-    info.filter( element =>{
-        return element.qType="Dimension";
-    })
-    .forEach( dimension =>{
-        return qix.getDimension(dimension.qId)
-    })
-    
-  //  return qix.getObject('KryeNN');
-})
-/*
-.then( obj =>{
-    return qix.getHypercubeData()
-})
-.then( data =>{
-    console.log(data);
-    data.forEach(element => {
-        element.qMatrix.forEach(row =>{
-            console.log(row);
-        })
-    });
-})
-*/
-.catch( error =>{
-    console.log("Errore : "+error);
+.then( () =>{
+    console.log(qix.getBookmarks())
 })
 
 
